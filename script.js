f839fedcac155c62873812f0d4d52cd640721361
Matheus Toledo <mtoledoz650@gmail.com>
2025-05-21 21:35:24 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const divLogoEpic = document.querySelector('.logo-epic')
 const logoEpic = document.getElementsByClassName('logo-epic')[0] //Pega a div do logo.
 const menuEpic = document.getElementsByClassName('list-container')[0] // Pega o menu escondido no icone.
 const navDesktop = document.getElementsByClassName('list-desktop') // Pega a nav do header.
@@ -9,15 +10,14 @@ const moreLi = document.getElementById('more')
 const moreMenu = document.getElementById('moreMenu')
 const logoFortnite = document.getElementById('logo-fort')
 const btnDown = document.getElementById('btn-download')
-
-// Variável para controlar o tempo de desaparecimento
-let hideTimeout; 
+const btnMobile = document.getElementById('menu-btn')
+const menuMobile = document.getElementById('menu-mobile')
 
 // Funçao de hover para aparecer o menu do Logo Epic.
 function menuOn () {
     // Abre ao passar o mouse por cima.
     logoEpic.addEventListener('mouseenter', () => {
-        menuEpic.style.display = "block"
+        menuEpic.style.display = "block";
     })
     
     // Fecha ao tirar o mouse de cima.
@@ -30,11 +30,11 @@ menuOn()
 
 function clickMenu () {
     // funcão para click.
-    logoEpic.addEventListener('click', () => {
+    divLogoEpic.addEventListener('click', () => {
         if (menuEpic.style.display === 'none' || menuEpic.style.display === '') {
             menuEpic.style.display = 'block';
         } else {
-            menuEpic.style.display= 'none'
+            menuEpic.style.display= 'none';
         }
 
         // Atualiza visibilidade do botão e do logo Fortnite
@@ -44,6 +44,22 @@ function clickMenu () {
 
 clickMenu()
 
+function clickMenuMobile () {
+    // funcão para click.
+    btnMobile.addEventListener('click', () => {
+        if (menuMobile.style.display === 'none' || menuMobile.style.display === '') {
+            menuMobile.style.display = 'block';
+        } else {
+            menuMobile.style.display= 'none';
+        }
+
+        // Atualiza visibilidade do botão e do logo Fortnite
+            disappearLogo();
+    })
+}
+
+clickMenuMobile()
+
 function disappear () {
     // Quando o menu estiver aberto desaparece o botao e o logo fortnite.
     if (menuEpic.style.display === 'block') {
@@ -52,6 +68,15 @@ function disappear () {
     } else {
         logoFortnite.style.display = 'block';
         btnDown.style.display = 'block';
+    }
+}
+
+function disappearLogo () {
+    // Quando o menu estiver aberto desaparece o logo epic.
+    if (menuMobile.style.display === 'block') {
+        divLogoEpic.style.display = 'none';
+    } else {
+        divLogoEpic.style.display = 'flex';
     }
 }
 
@@ -98,3 +123,13 @@ function moreMenuOn () {
 }
 
 moreMenuOn()
+
+// Detecta se o dispositivo suporta toque
+  const isTouchDevice = () => {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  };
+
+  // Adiciona classe no body apenas se NÃO for touch
+  if (!isTouchDevice()) {
+    document.body.classList.add('hover-enabled');
+  }
